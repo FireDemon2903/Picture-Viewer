62b8829b2994ea28b729c816a094413620d0b805
FireDemon2903 <95538183+FireDemon2903@users.noreply.github.com>
2023-01-19 18:44:20 +0100

    Update main.py
    Flyttede 'resize' ud til egen funktion og skrev lidt kommentarer.

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ import os
 # ^ ^ ^ ^ ^ Import ^ ^ ^ ^ ^
 
 
-# Laver en klasse som hedder "BilledeContainer" til at indeholde alle de billeder vi skal bruge samt metode til at vise dem.
+# Lav klassen "BilledKig" til widgets som skal vises til brugeren
 class BilledKig(ttk.Frame):
-    dirlist = os.listdir(
-        '.\\tis')
+    dirlist = os.listdir('.\\tis')
     print(dirlist)
     print(len(dirlist))
 
@@ -17,7 +16,7 @@ class BilledKig(ttk.Frame):
 
     def __init__(self, owner):
         super().__init__(owner)  # Her kalder vi superklassen, som er "ttk.Frame".
-        # Som argument sendes vores "App" klasse, som agerer som en beholder
+        # Som argument sendes vores "App" klasse, som agerer som en beholder/vores 'root'
 
         # Test label
         test_label = ttk.Label(owner, text="Hello World")
@@ -43,31 +42,23 @@ class BilledKig(ttk.Frame):
             self.curr_img_num += 1
 
             billede = Image.open(f".\\tis\\{dirlist[self.curr_img_num]}")  # path
-            img = ImageTk.PhotoImage(billede)  # convert image object to tkinter image
-            # ImageTk.getimage(img)  # get info about image (e.g. size)
+            
+            img_dim = self.resize_image(ImageTk.PhotoImage(billede))  # Skaf billedets demensioner. Hvis det ikke passer ind i beholderen, gør det mindre
+            img = ImageTk.PhotoImage(billede.resize(img_dim))  # Gør billedet mindre, hvis det ikke passer
 
-            # Work in progress
-            resize_width = False
-            resize_height = False
-            if img.width() <= 780: pass  # If width less than window
-            elif img.height() <= 500: pass  # If length less than window
-            else: img = ImageTk.PhotoImage(billede.resize((780, 500)))  # If dimensions are larger: resize
-            # TODO: Dynamic resizing (function)
+            self.curr_img = img  # Set nyt billede til at blive vist
+            self.billede_label = ttk.Label(owner, image=self.curr_img)  # Lav label til billedet
+            self.billede_label.grid(column=0, row=5, sticky=tk.NSEW, padx=5, pady=5, columnspan=5, rowspan=5)  # Formater
 
-            print('Width:', img.width(), ' Height', img.height())
-
-            self.curr_img = img
-            self.billede_label = ttk.Label(owner, image=self.curr_img)
-            self.billede_label.grid(column=0, row=5, sticky=tk.NSEW, padx=5, pady=5, columnspan=5, rowspan=5)
-
-            # test size change
+            # test size change:
             # owner.geometry('1200x800')
+            # size change works, but isn't implemented (yet?)
 
         except IndexError:
             self.curr_img_num = -1
             self.vis_neaste_billede(dirlist, owner)
 
-        except PermissionError:
+        except PermissionError:  # Hvis der er andre mapper i dirlist, ignorer dem og gå videre til naeste
             self.vis_neaste_billede(dirlist, owner)
 
     def vis_forrige_billede(self, dirlist, owner):  # Funktion til visning af forrige billede
@@ -78,32 +69,37 @@ class BilledKig(ttk.Frame):
                 self.curr_img_num = len(dirlist) - 1
 
 
-            billede = Image.open(f".\\tis\\{dirlist[self.curr_img_num]}")
-            img = ImageTk.PhotoImage(billede)
+            billede = Image.open(f".\\tis\\{dirlist[self.curr_img_num]}")  # Path
+            img_dim = self.resize_image(ImageTk.PhotoImage(billede))  # Skaf billedets demensioner. Hvis det ikke passer ind i beholderen, gør det mindre
+            img = ImageTk.PhotoImage(billede.resize(img_dim))  # Gør billedet mindre, hvis det ikke passer
+            
+            self.curr_img = img  # Set nyt billede til at blive vist
+            self.billede_label = ttk.Label(owner, image=self.curr_img)  # Lav label til billedet
+            self.billede_label.grid(column=0, row=5, sticky=tk.NSEW, padx=5, pady=5, columnspan=5, rowspan=5)  # Formater
 
-            if img.width() <= 780: pass  # If width less than window
-            elif img.height() <= 500: pass  # If length less than window
-            else: img = ImageTk.PhotoImage(billede.resize((780, 500)))  # If dimensions are larger: resize
-
-            self.curr_img = img
-            self.billede_label = ttk.Label(owner, image=self.curr_img)
-            self.billede_label.grid(column=0, row=5, sticky=tk.NSEW, padx=5, pady=5, columnspan=5, rowspan=5)
-
-        except PermissionError:
+        except PermissionError:  # Hvis der er andre mapper i dirlist, ignorer dem og gå videre til naeste
             self.vis_forrige_billede(dirlist, owner)
 
+    def zoom_billede(self):  # TODO
+        pass
+
     @staticmethod
-    def resize_image(picture):
-        if picture.width() <= 780:
-            pass  # If width less than window
-        elif picture.height() <= 500:
-            pass  # If length less than window
-        else:
-            img = ImageTk.PhotoImage(picture.resize((780, 500)))  # If dimensions are larger: resize
-        return picture.width(), picture.height()
+    def resize_image(image):  # Staticmethod, som returnerer et billedes demensioner, efter et check om den kan være i beholderen
+        img_width, img_height = image.width(), image.height()  # Get image dimensions
+        max_width, max_height = 780, 500  # Assign max width and height
+
+        resize_width = False
+        resize_height = False
+
+        if img_width >= max_width: resize_width = True  # If width greater than window
+        if img_height >= max_height: resize_height = True  # If length greater than window
+        if resize_width: img_width = max_width  # Change width to 780
+        if resize_height: img_height = max_height  # Change height to 500
+
+        return img_width, img_height
 
 
-# Laver en App klasse som indeholder vores root vindue.
+# Laver en 'App' klasse. Agerer som vores root vindue.
 class App(tk.Tk):
     def __init__(self):
         super().__init__()  # Arv fra tkinter
@@ -111,11 +107,8 @@ class App(tk.Tk):
         self.geometry("800x600")  # Demensioner på vindue
         self.resizable(False, False)  # Lås demensioner
 
-    def update(self) -> None:
-        self.geometry(self.test2)
-
 
 if __name__ == "__main__":
-    app = App()
-    BilledKig(app)
+    app = App()  # Instansiér App()
+    BilledKig(app)  # Instansiér BilledKig(), med app som arg(beholder)
     app.mainloop()  # Kør mainloop
